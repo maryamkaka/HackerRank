7aad33b75822f516d71fb63d0f9409f1b10b4a92
Maryam K <mkaka139@gmail.com>
2016-12-06 11:06:36 -0500

    Wrote find all paths function

--- Graphs/graph.py
+++ Graphs/graph.py
@@ -4,6 +4,16 @@ class graph(object):
     def __init__(self, g):
         self.graph = g
 
+    #string representation of graph
+    def __str__(self):
+        s = ''
+
+        for n in self.graph:
+            for x in self.graph[n]:
+                s += n + '->' + x + '\n'
+
+        return s
+
     #determine a path (uses backtracking)
     def find_path(self, start, end, path=[]):
         path = path + [start]
@@ -21,7 +31,22 @@ class graph(object):
         return None
 
     #find all paths
+    def find_all_paths(self, start, end, path=[]):
+        path = path + [start]
+
+        if(start == end):   #base case
+            return path
+        elif(not(start in self.graph) or not(end in self.graph)):
+            return None
+
+        paths = []
+        for n in self.graph[start]:
+            if(not(n in path)):
+                newpaths = self.find_all_paths(n, end, path)
+                paths.append(newpaths)
+        return paths
 
 
 g = graph({'A': ['B', 'C'], 'B': ['C', 'D'], 'C': ['D'], 'D': ['C'], 'E': ['F'], 'F': ['C']})
-print(g.find_path('A', 'G'))
+print(g)
+print(g.find_all_paths('A', 'C'))
